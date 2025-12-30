5f8ba1902a35efefdea2f321c324a674cb539241
RameshKumar077 <itsrk845418@gmail.com>
2025-12-31 02:31:40 +0530

    client: add vite react plugin, optimize react-router; send Bearer token in api interceptor

--- client1/src/services/api.js
+++ client1/src/services/api.js
@@ -1,13 +1,14 @@
 import axios from "axios";
 const API = axios.create({
     // baseURL: "http://localhost:5000/api"
+
     baseURL: "https://expenses-backend-ebon.vercel.app/api"
 });
 
 
 API.interceptors.request.use(req => {
     const token = localStorage.getItem("token");
-    if (token) req.headers.Authorization = token;
+    if (token) req.headers.Authorization = `Bearer ${token}`;
     return req;
 });
 
