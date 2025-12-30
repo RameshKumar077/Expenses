118773376943f81922808c0b42013dac226c41db
RameshKumar077 <itsrk845418@gmail.com>
2025-12-31 02:35:52 +0530

    Revert "client: add vite react plugin, optimize react-router; send Bearer token in api interceptor"
    This reverts commit 5f8ba1902a35efefdea2f321c324a674cb539241.

--- client1/src/services/api.js
+++ client1/src/services/api.js
@@ -1,14 +1,13 @@
 import axios from "axios";
 const API = axios.create({
     // baseURL: "http://localhost:5000/api"
-
     baseURL: "https://expenses-backend-ebon.vercel.app/api"
 });
 
 
 API.interceptors.request.use(req => {
     const token = localStorage.getItem("token");
-    if (token) req.headers.Authorization = `Bearer ${token}`;
+    if (token) req.headers.Authorization = token;
     return req;
 });
 
