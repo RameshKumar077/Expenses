15e7a31b6d0ad2636c43fb7b4e8e5378096a9239
RameshKumar077 <itsrk845418@gmail.com>
2025-12-31 17:42:38 +0530

    Updated files

--- client1/src/services/api.js
+++ client1/src/services/api.js
@@ -1,9 +1,8 @@
 import axios from "axios";
-
-const base = import.meta.env.VITE_API_URL || "http://localhost:5000";
 const API = axios.create({
-    baseURL: `${base}/api`
+    baseURL: "http://localhost:5000/api"
 });
+
 
 API.interceptors.request.use(req => {
     const token = localStorage.getItem("token");
