b935f5ceb03278562af407af8cbb8bf3b8ad3ad4
RameshKumar077 <itsrk845418@gmail.com>
2026-01-01 16:04:37 +0530

    Updated files

--- client1/src/services/api.js
+++ client1/src/services/api.js
@@ -2,7 +2,10 @@ import axios from "axios";
 
 // Prefer runtime environment variable VITE_API_URL (set in .env), fallback to localhost
 const API = axios.create({
-    baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api/v1"
+    // baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api/v1"
+
+
+    baseURL: import.meta.env.VITE_API_URL || "https://expenses-ten-phi.vercel.app/api/v1"
 });
 
 
